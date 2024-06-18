947071bc88ae926a4434cd4eacd0c22edacf56cf
Erdene-Ochir <erdene-ochir@Erdene-Ochirs-MacBook-Air.local>
2024-06-18 08:13:33 +0100

    added celery

--- deploy.py
+++ deploy.py
@@ -6,15 +6,22 @@ from celery import Celery
 CELERY_IP = os.environ["CELERY_IP"]
 CELERY_PORT = os.environ["CELERY_PORT"]
 CELERY_NAME = os.environ["CELERY_NAME"]
+CELERY_WORKER = os.environ["CELERY_WORKER"]
+CELERY_TASK = os.environ["CELERY_TASK"]
 
 celery_app = Celery(
     CELERY_NAME, broker=f"redis://{CELERY_IP}:{CELERY_PORT}", backend=f"redis://{CELERY_IP}:{CELERY_PORT}"
 )
+
+def deploy_public_model(changed_models):
+    celery_job = celery_app.send_task(f"{CELERY_WORKER}.{CELERY_TASK}", args=([changed_models]))
+    return True
+
 
 if __name__ == "__main__":
     changed_files = sys.argv[1:]
     changed_models = [Path(x).parents[-3] for x in changed_files if x.startswith("models/")]
     changed_models = list(set(changed_models))
     print(changed_models)
-    # if changed_models != []:
-    #     pipeline(changed_models)
+    if changed_models != []:
+        deploy_public_model(changed_models)
